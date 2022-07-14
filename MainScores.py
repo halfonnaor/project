ce93f1f3cf8c4afb157eb014681bd0b6ab0b2024
halfonnaor <halfonnaor@gmail.com>
2022-07-14 19:49:10 +0300

    new6

--- MainScores.py
+++ MainScores.py
@@ -23,4 +23,5 @@ def about():
 #     return open(scoresText).read(), 200
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True, port=30001)
+    #app.run('0.0.0.0', debug=True, port=30001)
+    app.run(debug=True, host='0.0.0.0' , port=5002)
